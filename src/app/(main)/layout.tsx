adb3a0c4c9eb53847cbbff69659476411c50d883
Ethan Huang 黃靖緯 <69299174+chingweih@users.noreply.github.com>
2024-05-11 11:14:47 +0800

    add vercel speed insight

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,5 +1,6 @@
 import Navigation from '../_components/Navigation'
 import UserHeader from '../_components/UserHeader'
+import { SpeedInsights } from '@vercel/speed-insights/next'
 
 export default function MainLayout({
   children,
@@ -8,6 +9,7 @@ export default function MainLayout({
 }) {
   return (
     <>
+      <SpeedInsights />
       <Navigation />
       <UserHeader />
       <div className='p-3'>{children}</div>
