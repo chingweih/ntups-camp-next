5740b76055b4d1561bd11579ae2a78a7ae498980
Ethan Huang 黃靖緯 <69299174+chingweih@users.noreply.github.com>
2024-06-08 17:07:50 +0800

    unoptimize gif

--- src/app/_components/VotingGIF.tsx
+++ src/app/_components/VotingGIF.tsx
@@ -2,5 +2,13 @@ import voting from '@/assets/voting.gif'
 import Image from 'next/image'
 
 export default function Voting({ size }: { size: number }) {
-  return <Image src={voting} alt='Voting' width={size} height={size} />
+  return (
+    <Image
+      src={voting}
+      alt='Voting'
+      width={size}
+      height={size}
+      unoptimized={true}
+    />
+  )
 }
