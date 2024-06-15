5114c7ca6da00193111727b10778ef3ea74bc93b
Ethan Huang 黃靖緯 <69299174+chingweih@users.noreply.github.com>
2024-06-15 23:34:31 +0800

    front end tweaks

--- src/app/(admin)/admin/task/TaskTable.tsx
+++ src/app/(admin)/admin/task/TaskTable.tsx
@@ -22,6 +22,8 @@ import { Tables } from '@/utils/database.types'
 import JSZip from 'jszip'
 import { saveAs } from 'file-saver'
 import Link from 'next/link'
+import { useState } from 'react'
+import { toast } from 'sonner'
 
 export type TasksWithFiles = Tables<'tasks'> & {
   files:
@@ -45,101 +47,125 @@ export default function TaskTable({ tasks }: { tasks: TasksWithFiles[] }) {
       </TableHeader>
       <TableBody>
         {tasks.map((task) => (
-          <Collapsible key={task.id} asChild>
-            <>
-              <TableRow>
-                <TableCell>{getDateString(task.due_datetime)}</TableCell>
-                <TableCell>{task.name}</TableCell>
-                <TableCell>
-                  <Badge>{task.for_team || '所有'}</Badge>
-                </TableCell>
-                <TableCell>
-                  <div className='flex flex-row'>
-                    <CollapsibleTrigger asChild>
-                      <Button variant='link'>查看檔案</Button>
-                    </CollapsibleTrigger>
-                    <Button
-                      variant='link'
-                      onClick={async () => {
-                        let zip = new JSZip()
-
-                        const files = task.files?.map(async (file) => {
-                          const downloadFile = await fetch(
-                            `/admin/task/download/${file.file.file_url}`
-                          )
-
-                          const blob = await downloadFile.blob()
-                          return {
-                            blob,
-                            fileName: `${task.name}-${
-                              file.user?.displayName ||
-                              file.user?.email?.split('@')[0]
-                            }.pdf`,
-                          }
-                        })
-
-                        const blobs = await Promise.all(files!)
-
-                        blobs.forEach((blob) => {
-                          zip.file(blob.fileName, blob.blob)
-                        })
-
-                        zip.generateAsync({ type: 'blob' }).then((content) => {
-                          saveAs(content, `【政治營檔案】${task.name}.zip`)
-                        })
-                      }}
-                    >
-                      下載全部
-                    </Button>
-                  </div>
-                </TableCell>
-              </TableRow>
-              <CollapsibleContent asChild>
-                <>
-                  {task.files ? (
-                    task.files.map((file) => (
-                      <TableRow key={file.file.file_url}>
-                        <TableCell>
-                          <div className='flex flex-row items-center justify-start gap-3'>
-                            <UserAvatar
-                              user={file.user}
-                              userDisplayName={file.user?.displayName || ''}
-                            />
-                            <div className='flex flex-col items-start justify-center'>
-                              <p>{file.user?.displayName}</p>
-                              <p className='text-slate-500 text-xs'>
-                                {file.user?.email?.split('@')[0]}
-                              </p>
-                            </div>
-                          </div>
-                        </TableCell>
-                        <TableCell colSpan={2}>
-                          最後上傳時間：
-                          {getDateString(file.file.created_at)}
-                        </TableCell>
-                        <TableCell>
-                          <Button variant='link'>
-                            <Link
-                              href={`/admin/task/download/${file.file.file_url}`}
-                              target='_blank'
-                            >
-                              下載
-                            </Link>
-                          </Button>
-                        </TableCell>
-                      </TableRow>
-                    ))
-                  ) : (
-                    <TableRow>
-                      <TableCell colSpan={4}>尚未有檔案</TableCell>
-                    </TableRow>
-                  )}
-                </>
-              </CollapsibleContent>
-            </>
-          </Collapsible>
+          <TaskRow task={task} key={task.id} />
         ))}
       </TableBody>
     </Table>
+  )
+}
+
+function TaskRow({ task, key }: { task: TasksWithFiles; key?: number }) {
+  const [downloading, setDownloading] = useState(false)
+
+  return (
+    <Collapsible key={key!} asChild>
+      <>
+        <TableRow>
+          <TableCell>{getDateString(task.due_datetime)}</TableCell>
+          <TableCell>{task.name}</TableCell>
+          <TableCell>
+            <Badge>{task.for_team || '所有'}</Badge>
+          </TableCell>
+          <TableCell>
+            <div className='flex flex-row'>
+              <CollapsibleTrigger asChild>
+                <Button variant='link'>查看檔案</Button>
+              </CollapsibleTrigger>
+              <Button
+                variant='link'
+                disabled={downloading}
+                onClick={async () => {
+                  setDownloading(true)
+                  toast.loading(`正在下載｜${task.name}`, {
+                    id: `downloading-${task.id}`,
+                    duration: Infinity,
+                  })
+                  let zip = new JSZip()
+
+                  const files = task.files?.map(async (file) => {
+                    const downloadFile = await fetch(
+                      `/admin/task/download/${file.file.file_url}`
+                    )
+
+                    const blob = await downloadFile.blob()
+                    return {
+                      blob,
+                      fileName: `${task.name}-${
+                        file.user?.displayName ||
+                        file.user?.email?.split('@')[0]
+                      }.pdf`,
+                    }
+                  })
+
+                  if (!files) {
+                    toast.dismiss(`downloading-${task.id}`)
+                    toast.error('檔案下載失敗或是沒有檔案')
+                    setDownloading(false)
+                    return
+                  }
+
+                  const blobs = await Promise.all(files)
+
+                  blobs.forEach((blob) => {
+                    zip.file(blob.fileName, blob.blob)
+                  })
+
+                  zip.generateAsync({ type: 'blob' }).then((content) => {
+                    saveAs(content, `【政治營檔案】${task.name}.zip`)
+                    setDownloading(false)
+                    toast.dismiss(`downloading-${task.id}`)
+                    toast.success(`下載完成｜${task.name}`)
+                  })
+                }}
+              >
+                下載全部
+              </Button>
+            </div>
+          </TableCell>
+        </TableRow>
+        <CollapsibleContent asChild>
+          <>
+            {task.files ? (
+              task.files.map((file) => (
+                <TableRow key={file.file.file_url}>
+                  <TableCell>
+                    <div className='flex flex-row items-center justify-start gap-3'>
+                      <UserAvatar
+                        user={file.user}
+                        userDisplayName={file.user?.displayName || ''}
+                      />
+                      <div className='flex flex-col items-start justify-center'>
+                        <p>{file.user?.displayName}</p>
+                        <p className='text-slate-500 text-xs'>
+                          {file.user?.email?.split('@')[0]}
+                        </p>
+                      </div>
+                    </div>
+                  </TableCell>
+                  <TableCell colSpan={2}>
+                    最後上傳時間：
+                    {getDateString(file.file.created_at)}
+                  </TableCell>
+                  <TableCell>
+                    <Button variant='link'>
+                      <Link
+                        href={`/admin/task/download/${file.file.file_url}`}
+                        target='_blank'
+                      >
+                        下載
+                      </Link>
+                    </Button>
+                  </TableCell>
+                </TableRow>
+              ))
+            ) : (
+              <TableRow>
+                <TableCell colSpan={4}>尚未有檔案</TableCell>
+              </TableRow>
+            )}
+          </>
+        </CollapsibleContent>
+      </>
+    </Collapsible>
   )
 }
