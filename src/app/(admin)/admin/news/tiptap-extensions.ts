3f60df0f82c00beaeb3bf9befc06b83ca0c6cbe7
Ethan Huang 黃靖緯 <69299174+chingweih@users.noreply.github.com>
2024-06-19 01:06:45 +0800

    update extensions again

--- src/app/(admin)/admin/news/tiptap-extensions.ts
+++ src/app/(admin)/admin/news/tiptap-extensions.ts
@@ -2,5 +2,6 @@ import Image from '@tiptap/extension-image'
 import Link from '@tiptap/extension-link'
 import Underline from '@tiptap/extension-underline'
 import StarterKit from '@tiptap/starter-kit'
+import TextStyle from '@tiptap/extension-text-style'
 
-export const customExtensions = [Link, Image, Underline, StarterKit]
+export const customExtensions = [Link, Image, Underline, StarterKit, TextStyle]
